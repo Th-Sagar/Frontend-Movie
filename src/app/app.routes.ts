c1bcf015ddb1b99efbb89f139de073cabfb1740c
Th-Sagar <sagarthapa98122@gmail.com>
2025-03-29 12:14:17 +0545

    feat: navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Movie App - Home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    title: 'Movie App - Login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    title: 'Movie App - Register',
+    component: RegisterComponent,
+  },
+];
