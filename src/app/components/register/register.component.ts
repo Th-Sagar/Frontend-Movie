c773e534e5516792e85416e8706deee2c38dbb9b
Th-Sagar <sagarthapa98122@gmail.com>
2025-03-29 12:27:21 +0545

    feat: register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  name = new FormControl<string>('', [Validators.required]);
+  username = new FormControl<string>('', [Validators.required]);
+  email = new FormControl<string>('', [Validators.required, Validators.email]);
+  password = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(5),
+  ]);
 
+  registerForm: FormGroup;
+
+  inlineNotification = {
+    show: false,
+    type: '',
+    text: '',
+  };
+
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password,
+    });
+  }
+
+  register() {}
 }
