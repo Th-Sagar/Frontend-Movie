db384a71d57270d0b9d0956fde604a31b096fb90
Th-Sagar <sagarthapa98122@gmail.com>
2025-03-30 15:16:35 +0545

    setup: update-movie

---
+++ src/app/components/update-movie/update-movie.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-movie',
+  imports: [],
+  templateUrl: './update-movie.component.html',
+  styleUrl: './update-movie.component.css'
+})
+export class UpdateMovieComponent {
+
+}
